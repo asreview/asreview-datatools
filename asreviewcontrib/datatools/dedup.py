4b7fc0bb81dba8b8f105e652b922cba7b9823aed
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-09-26 11:12:58 +0200

    Update dedup.py
    only strip and replace in case of string

--- asreviewcontrib/datatools/dedup.py
+++ asreviewcontrib/datatools/dedup.py
@@ -1,15 +1,14 @@
 import argparse
 
 import pandas as pd
+from pandas.api.types import is_string_dtype
 
 
 def dedup(asdata, pid='doi'):
     if pid in asdata.df.columns:
-        # replace NaN values with empty string to prevent astype(str) from creating literal 'NaN' string
-        asdata.df[pid] = asdata.df[pid].fillna('')
-
-        # convert to string to support non-string types, strip whitespaces and replace empty strings with NaN values
-        s_pid = asdata.df[pid].astype(str).str.strip().replace("", None)
+        # in case of strings, strip whitespaces and replace empty strings with None
+        if is_string_dtype(asdata.df[pid]):
+            s_pid = asdata.df[pid].astype(str).str.strip().replace("", None)
 
         # remove records based on duplicate PIDs
         asdata.df = asdata.df[(~s_pid.duplicated()) | (s_pid.isnull())].reset_index(drop=True)
@@ -29,6 +28,7 @@ def _parse_arguments_dedup():
     parser = argparse.ArgumentParser(prog="asreview data dedup")
     parser.add_argument("input_path", type=str, help="The file path of the dataset.")
     parser.add_argument("--output_path", "-o", default=None, type=str, help="The file path of the dataset.")
-    parser.add_argument("--pid", default='doi', type=str, help="Persistent identifier used for deduplication. Default: doi.")
+    parser.add_argument("--pid", default='doi', type=str,
+                        help="Persistent identifier used for deduplication. Default: doi.")
 
     return parser
