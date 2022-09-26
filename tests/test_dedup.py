5b017fa273d9a940104cd61d2863f452a3b75056
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-09-26 09:08:40 +0200

    Update test_dedup.py
    Remove np.nan from test

--- tests/test_dedup.py
+++ tests/test_dedup.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 import asreview
 from asreviewcontrib.datatools.dedup import dedup
@@ -8,19 +7,18 @@ from asreviewcontrib.datatools.dedup import dedup
 def test_dedup():
     df_dups = asreview.ASReviewData(
         pd.DataFrame({
-            "title": ["a", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"],
-            "abstract": ["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem",
-                         "lorem", "lorem"],
-            "doi": ["10.1", "10.2", "10.3", "10.3", "", "", "   ", "   ", np.nan, np.nan, None, None]
+            "title": ["a", "a", "b", "c", "d", "e", "f", "g", "h", "i"],
+            "abstract": ["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem"],
+            "doi": ["10.1", "10.2", "10.3", "10.3", "", "", "   ", "   ", None, None]
         })
     )
 
     df_nodups = asreview.ASReviewData(
         pd.DataFrame({
-            "title": ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k"],
-            "abstract": ["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem"],
-            "doi": ["10.1", "10.3", "", "", "   ", "   ", "", "", "", ""]
+            "title": ["a", "b", "d", "e", "f", "g", "h", "i"],
+            "abstract": ["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem"],
+            "doi": ["10.1", "10.3", "", "", "   ", "   ", "", ""]
         })
     )
 
-    assert dedup(df_dups).df.equals(df_nodups.df)
+    assert pd.testing.assert_frame_equal(dedup(df_dups).df, df_nodups.df)
