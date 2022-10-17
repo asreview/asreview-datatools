675f3c45cb59aefb7a829bda3907a0dee15887b4
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-10-17 14:55:34 +0200

    add specific stack tests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-import pytest
-
 from asreview.data import ASReviewData
 from asreviewcontrib.datatools.stack import stack
 
@@ -15,6 +13,8 @@ def test_stack(tmpdir):
     output_path = Path(tmpdir, "test_output.ris")
     stack(output_path, [file_1, file_2])
     as_test = ASReviewData.from_file(output_path)
-    print(as_test.df)
-    print("yay?")
-    assert 1 == 1
+
+    assert len(as_test.df) == 14
+    assert as_test.df['included'].value_counts()[-1] == 9
+    assert as_test.df['included'].value_counts()[0] == 3
+    assert as_test.df['included'].value_counts()[1] == 2
